72ca8b259259b0ca087ae2efb1722dfe4c42fefb
vbchin2 <Vijay.Chintalapati@gmail.com>
2013-06-30 18:06:05 -0400

    Correct the path and executable script call

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 export PWD=`pwd`
-export PATH=$PATH;$PWD/rhq-bundle-deployer-4.8.0/bin
-rhq-ant.sh -propertyfile deployment.properties
+export PATH=$PATH:$PWD/rhq-bundle-deployer-4.8.0/bin
+rhq-ant -propertyfile deployment.properties
